27d6367007df574ab1240a7ed366cfc48b1d88a2
Moshe Piamenta <imoshep@gmail.com>
2020-09-24 20:15:19 +0300

    port to PORT

--- puppeteer-server/puppeteer-server.js
+++ puppeteer-server/puppeteer-server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 const cors = require('cors');
 
-const port = process.env.port || 8181;
+const port = process.env.PORT || 8181;
 
 const app = express(); 
 
